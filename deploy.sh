960b0850d39872ac39704431acdfcb224d2ff96f
xiyuanyang-code <xiyuan__yang@outlook.com>
2025-06-29 13:03:43 +0800

    fix: rename and fux bugs in bash scripts

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ nohup python3 ./src/backend/app.py > ./log/backend.log 2>&1 &
 
 # Start frontend (simple HTTP server) on port 8000
 echo "Starting frontend on port 8000..."
-cd ./src/frontend
+cd ./src/frontend || echo "Error, no such files or directories"
 nohup python3 -m http.server 8000 > ../../log/frontend.log 2>&1 &
 
 echo "Backend: http://127.0.0.1:5000"
